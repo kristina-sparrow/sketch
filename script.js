74fb8f273b1ac786750f1023de9fe1de6de5e633
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2022-12-15 09:59:25 -0700

    Create mouse down and color selection functionality

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 let buttons = document.querySelectorAll("button");
+let container = document.querySelector(".container");
+let cells = document.querySelectorAll(".cell");
+let color = document.querySelector("#colorpicker");
 let gridToggleBtn = document.querySelector(".gridline-toggle");
 
 buttons.forEach(button => {
-    button.addEventListener("click", function () {
+    button.addEventListener("click", () => {
         createGrid(button.id);
     })
-})
+});
 
 function createGrid (size) {
     resetGrid();
     document.documentElement.style.setProperty('--gridSize', size);
-    let container = document.querySelector(".container");
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let div = document.createElement("div");
-            div.classList.add("grid-square");
+            div.classList.add("cell");
             container.appendChild(div);
         }
     }
+    let currentCells = document.querySelectorAll(".cell");
+    let currentColor = document.querySelector("#colorpicker");
+    colorMouseDown(currentCells, currentColor);
 }
 
 function resetGrid () {
-    let container = document.querySelector(".container");
     container.querySelectorAll('*').forEach(function(child) {
         child.remove();
     });
 }
 
-gridToggleBtn.addEventListener("change", function () {
-    toggleGridlines();
-})
+gridToggleBtn.addEventListener("change", () => {
+    let currentCells = document.querySelectorAll(".cell");
+    toggleGridlines(currentCells);
+});
 
-function toggleGridlines () {
-    let cells = document.querySelectorAll(".grid-square");
+function toggleGridlines (cells) {
     cells.forEach(cell => {
         cell.classList.toggle("no-gridlines");
     });
 }
+
+function colorMouseDown (cells, color) {
+    let isMouseDown = false;
+    cells.forEach(cell => {
+        cell.addEventListener("mousedown", () => {
+            isMouseDown = true;
+        });
+        cell.addEventListener("mouseup", () => {
+            isMouseDown = false;
+        });
+        cell.addEventListener("mousemove", (event) => {
+            if (isMouseDown) {
+                cell.style.backgroundColor = color.value;
+            }
+        });
+    });
+} 
 
 createGrid(16);
